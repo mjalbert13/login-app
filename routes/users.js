4271b843e8f0f67a012ba15eb798e08c806568bf
mjalbert13 <mjalbert13@gmail.com>
2019-07-29 14:08:17 -0400

    adding mongo register form

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,70 @@
 const express = require("express");
-
+const bcrypt = require('bcryptjs');
 const router = express.Router();
+const User = require("../model/User");
 
-router.get('/register', (req,res) => res.send('register'));
+router.get('/register', (req,res) => res.render('register'));
 
-router.get('/login', (req,res) => res.send('login'));
+router.get('/login', (req,res) => res.render('login'));
+
+//Register Handle
+router.post('/register', (req,res) =>{
+   const {name, email, password, password2} = req.body;
+   let errors =[];
+
+   if(!name || !email || !password || !password2){
+       errors.push({msg: 'You need to fill in all fields'});
+   }
+
+   if(!password !== !password2){
+       errors.push({msg:'Your passwords did not match'});
+   }
+
+   if(password.length < 6){
+       errors.push({msg:'Password must be greater than 6 chars'});
+   }
+
+   if(errors.length > 0){
+       res.render('register',{
+           errors,
+           name,
+           email,
+           password,
+           password2
+       });
+   }else{
+       User.findOne({email: email})
+       .then(function(user){
+           if(user) {
+            errors.push({msg:'Email already exists'})
+            res.render('register',{
+                errors,
+                name,
+                email,
+                password,
+                password2
+            });
+           } else{
+               const newUser = new User({
+                   name: name,
+                   email: email,
+                   password: password
+               });
+               bcrypt.genSalt(10, (err, salt) => 
+                bcrypt.hash(newUser.password, salt,(err, hash)=>{
+                    if(err) throw err;
+
+                    newUser.password = hash;
+
+                    newUser.save()
+                    .then(user =>{
+                        res.redirect('/login');
+                    })
+                    .catch( err => console.log(err));
+                }));
+           }
+       });
+   }
+});
 
 module.exports =router;
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require('mongoose');
 
 const app = express();
+
+//Mongo DB Set up
+const db = require('./model');
+mongoose.connect("mongodb://localhost/loginApp", { useNewUrlParser: true})
 
 //EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+//Body parser
+app.use(express.urlencoded({extended:false}));
 
 //Routes
 app.use("/", require("./routes/index"));
